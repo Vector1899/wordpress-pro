2d16f53d5521a3017173527c8e52b0a6e05a4507
Vector1899 <Richardfulldeveloper1899@gmail.com>
2020-04-16 09:18:20 +0300

    Move WP Cron to OS Cron (Task #15846)

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,8 +1,3 @@
-# @Author: sotirisky
-# @Date:   2020-03-31 09:45:10
-# @Last Modified by:   sotirisky
-# @Last Modified time: 2020-03-31 09:46:03
-
 #!/usr/bin/env bash
 
 DIR="$( cd "$(dirname $( dirname "${BASH_SOURCE[0]}" ) )" >/dev/null && pwd )"
