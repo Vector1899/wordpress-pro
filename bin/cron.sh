def9996bae6a1e9aafa9b07a73850079529fc161
Vector1899 <Richardfulldeveloper1899@gmail.com>
2020-12-02 10:46:06 +0200

    Fixed cron.sh permissions (Task #18408)

--- bin/cron.sh
+++ bin/cron.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$(dirname $( dirname "${BASH_SOURCE[0]}" ) )" >/dev/null && pwd )"
 
-$DIR/vendor/bin/wp --allow-root --path=webroot/wp cron event run --due-now
+$DIR/vendor/bin/wp --allow-root --path=$DIR/webroot/wp cron event run --due-now
